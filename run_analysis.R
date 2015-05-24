544aa8426e5562c8c44bf7d6fbc1e1e75d5ae29c
crossaw <ammmj@ez2.net>
2015-05-23 17:34:53 -0700

    Refined the script and added comments

--- run_analysis.R
+++ run_analysis.R
@@ -1,30 +1,46 @@
-library(data.table)
+# run_analysis.R
 
-#datFolder <- "dat"
-datFolder <- "UCI HAR Dataset"
-#datFolder <- "."
+# This script calculates the mean of certain measurements in the UCI Human
+# Activity Recognition dataset, grouped by activity and volunteer (subject).
+# The selected measurements are the ones identified as the mean, std and
+# meanFreq values.
+
+# The parts of the script which correspond to the tasks numbered 1 through 5 in
+# the project instruction are identified by --X--, where X is a number.  In
+# some cases more than one line or section of code are associated with a task.
+
+# See CodeBook.md for details of the output.
+
+### USER OPTIONS
+# Set datFolder to the root location of the dataset.
+   #datFolder <- "dat"
+   datFolder <- "UCI HAR Dataset"
+   #datFolder <- "."
+
+# Set niceSet to T if you want the niceSet output (see CodeBook.md)
+   niceSet <- F
+
+library(data.table)
 
 "%.%" <- function(a, b) {paste(a, b, sep="")} # used on filenames in read.table
 
-### Preparation for Naming Cols and Rows
+### PREPARATION FOR NAMING COLUMNS AND ROWS
 # All names with capital M in "Mean" are angle measurements, not means
 features <- gsub("\\()|-|Mean", "", read.table(datFolder%.%"/features.txt")[,2])
-features <- tolower( features )
-   # the measurement names, cleaned                        (char vector)
+features <- tolower( features )                                      # --4--
+   # the measurement names, cleaned                    (char vector)
 
-colsOfInterest <- grep("mean|std", features)
-   # indices of the measurements that we care about        (int vector)
+colsOfInterest <- grep("mean|std", features)                         # --2--
+   # indices of the measurements that we care about    (int vector)
 
-#2345678901234567890123456789012345678901234567890123456789012345678901234567890
-#        1         2         3         4         5         6         7         |
 filename <- datFolder %.% "/activity_labels.txt"
 actLabels <- sub( "_", "", tolower(as.character( read.table(filename)[,2] )) )
-   # activity names in numerical order                     (char vector)
+   # activity names in numerical order                 (char vector)   --3--
 
-### Manipulation of Test Data
+### MANIPULATION OF TEST DATA
 # Read the data for the measurements that we care about
 filename <- datFolder %.% "/test/X_test.txt"
-testDF <- read.table(filename, colClasses="numeric")[,colsOfInterest]
+testDF <- read.table(filename, colClasses="numeric")[,colsOfInterest] # --2--
 
 # Put subject numbers in what will become the second column
 testDF <- cbind( read.table(datFolder %.% "/test/subject_test.txt"), testDF )
@@ -33,10 +49,10 @@ testDF <- cbind( read.table(datFolder %.% "/test/subject_test.txt"), testDF )
 testAct <- as.integer( read.table(datFolder %.% "/test/Y_test.txt")[,1] )
 testDF <- cbind(actLab = actLabels[testAct], testDF)
 
-### Manipulation of Training Data
+### MANIPULATION OF TRAINING DATA
 # Read the data for the measurements that we care about
 filename <- datFolder %.% "/train/X_train.txt"
-trainDF <- read.table(filename, colClasses="numeric")[,colsOfInterest]
+trainDF <- read.table(filename, colClasses="numeric")[,colsOfInterest] # --2--
 
 # Put subject numbers in what will become the second column
 trainDF <- cbind( read.table(datFolder%.%"/train/subject_train.txt"), trainDF )
@@ -45,22 +61,24 @@ trainDF <- cbind( read.table(datFolder%.%"/train/subject_train.txt"), trainDF )
 trainAct <- as.integer( read.table(datFolder %.% "/train/Y_train.txt")[,1] )
 trainDF <- cbind(actLab=actLabels[trainAct], trainDF)
 
-### Merge the data sets and label the columns
-XDF <- rbind(testDF, trainDF)
-   # the merged, semi-raw, mean and std measurements       (data table)
-names(XDF) <- c("activity", "subject", features[colsOfInterest])
-tvec = interaction(XDF[2:1])                         # used as index in tapply
-vtapply <- function(...) {as.vector( tapply(...) )}  # function for lapply
+### CREATION OF OUTPUT
+XDF <- rbind(testDF, trainDF)                                           # --1--
+   # the merged mean and std measurements from the input files    (data table)
+names(XDF) <- c("activity", "subject", features[colsOfInterest])        # --4--
+tvec = interaction(XDF[2:1])        # used as index in tapply             --5--
 activity <- factor(tapply( XDF[,1], tvec, unique ), labels= levels( XDF[,1] ))
+vtapply <- function(...) {as.vector( tapply(...) )}       # function for lapply
 tidySet <- data.frame( activity, lapply(XDF[2:ncol(XDF)], vtapply, tvec, mean) )
 rownames(tidySet) <- NULL
 
-#niceSet <-lapply(XDF[3:ncol(XDF)], tapply, list(XDF$subject,XDF$activity), mean)
-   # mean of each measurement by subject and activity      (list of matrices)
+if (niceSet) {                    # Can be set to T at the top of the script
+   tlist <- list(XDF$subject, XDF$activity)
+   niceSet <- lapply(XDF[3:ncol(XDF)], tapply, tlist, mean)
+      # mean of each measurement by subject and activity     (list of matrices)
+}
+write.table(tidySet, file = "TidyHumanActivity.txt", row.names=FALSE)
 
-### Clean the Workspace
+### CLEANUP
 # Leave only XDF, niceSet and tidySet
 rm(features, colsOfInterest, actLabels, testDF, testAct, trainDF, trainAct,
-      activity, tvec, vtapply)
-
-write.table(tidySet, file = "TidyHumanActivity.txt", row.names=FALSE)
+      activity, tvec, tlist, vtapply, datFolder, filename, "%.%")
